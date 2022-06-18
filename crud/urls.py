cc4edc4e6df10b4befdaccdb6436868e24f67f5f
mohangundluri <89716603+mohangundluri@users.noreply.github.com>
2022-06-18 12:31:39 +0530

    Update urls.py

--- crud/urls.py
+++ crud/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('feedback.urls')),
     path('payment/', include('gateway.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
